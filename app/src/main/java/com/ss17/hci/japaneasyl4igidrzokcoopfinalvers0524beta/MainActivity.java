d930bd130a7ced8886eb5e5a7f6a15d07fb054f6
L4igi <l.hoewarth@gmail.com>
2017-05-04 20:12:13 +0200

    Worked

--- app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/MainActivity.java
+++ app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -39,6 +40,8 @@ public class MainActivity extends AppCompatActivity
         Homefragment homefragment=new Homefragment();
         FragmentManager fragmentManager= getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.fragment, homefragment).commit();
+
+
     }
 
     @Override
@@ -114,4 +117,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
--- app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/SettingsFrag.java
+++ app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/SettingsFrag.java
@@ -1,10 +1,14 @@
 package com.ss17.hci.japaneasyl4igidrzokcoopfinalvers0524beta;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,5 +26,7 @@ public class SettingsFrag extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+
     }
+
 }
