b557a313af6bf117d69b134edab910c4e8ff420e
L4igi <l.hoewarth@gmail.com>
2017-05-05 14:35:05 +0200

    Worked

--- app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/DictionaryFrag.java
+++ app/src/main/java/com/ss17/hci/japaneasyl4igidrzokcoopfinalvers0524beta/DictionaryFrag.java
@@ -1,51 +1,103 @@
 package com.ss17.hci.japaneasyl4igidrzokcoopfinalvers0524beta;
 
 
+import android.app.ExpandableListActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ExpandableListView;
-import android.widget.ListView;
-
-
 /**
- * A simple {@link Fragment} subclass.
+ * Pieced together from:
+ * Android samples: com.example.android.apis.view.ExpandableList1
+ * http://androidword.blogspot.com/2012/01/how-to-use-expandablelistview.html
+ * http://stackoverflow.com/questions/6938560/android-fragments-setcontentview-alternative
+ * http://stackoverflow.com/questions/6495898/findviewbyid-in-fragment-android
  */
 public class DictionaryFrag extends Fragment {
 
-    public DictionaryFrag() {
-        // Required empty public constructor
-    }
-
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-
-        View view = inflater.inflate(R.layout.fragment_dictionary, container, false);
-
-        String [] menutItems = {"Pikachu", "DrZok", "L4igi"};
-
-        ListView listView = (ListView) view.findViewById(R.id.list);
-
-        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                android.R.layout.simple_list_item_1,
-                menutItems
-        );
-
-        listView.setAdapter(listViewAdapter);
-
-
-        return view;
-
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_dictionary, null);
+        ExpandableListView elv = (ExpandableListView) v.findViewById(R.id.list);
+        elv.setAdapter(new SavedTabsListAdapter());
+        return v;
     }
 
+    public class SavedTabsListAdapter extends BaseExpandableListAdapter {
 
+        private String[] groups = { "Basics", "Park", "Restaurant", "University" };
+
+        private String[][] children = {
+                { "Guten Morgen", "Hallo", "Bis später", "Bitte", "Danke" },
+                { "Baum", "Bank", "Gras", "Sonnenschein" },
+                { "Wein", "Rechnung", "Fisch" },
+                { "Lernen", "Vorlesung", "Kurs" }
+        };
+
+        @Override
+        public int getGroupCount() {
+            return groups.length;
+        }
+
+        @Override
+        public int getChildrenCount(int i) {
+            return children[i].length;
+        }
+
+        @Override
+        public Object getGroup(int i) {
+            return groups[i];
+        }
+
+        @Override
+        public Object getChild(int i, int i1) {
+            return children[i][i1];
+        }
+
+        @Override
+        public long getGroupId(int i) {
+            return i;
+        }
+
+        @Override
+        public long getChildId(int i, int i1) {
+            return i1;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+        @Override
+        public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(DictionaryFrag.this.getActivity());
+            textView.setText(getGroup(i).toString());
+            return textView;
+        }
+
+        @Override
+        public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+            TextView textView = new TextView(DictionaryFrag.this.getActivity());
+            textView.setText(getChild(i, i1).toString());
+            return textView;
+        }
+
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return true;
+        }
+
+    }
 
 }
